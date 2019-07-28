45ba6e906f491befd62534c63dd588c4e6f19bb2
Zach Parks <zach@alliware.com>
2019-07-28 16:54:09 -0500

    setup addExpense page and expense form to submit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
 
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
