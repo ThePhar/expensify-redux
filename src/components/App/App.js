64a0e80bf9d3d7450cfb5ea55930f67eb98489b4
Zach Parks <zach@alliware.com>
2019-07-27 19:23:59 -0500

    added "404"-like functionality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.scss";
 
@@ -8,15 +8,17 @@ const DashboardPage = () => ( <div>This is from my dashboard component.</div> );
 const AddExpensePage = () => ( <div>This is from my add expense component.</div> );
 const EditExpensePage = () => ( <div>This is from my edit expense component.</div> );
 const HelpPage = () => ( <div>This is from my help component.</div> );
+const NotFoundPage = () => (<div>404!</div>);
 
 const App = () => (
     <BrowserRouter>
-        <Fragment>
+        <Switch>
             <Route path="/" component={DashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
-        </Fragment>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
