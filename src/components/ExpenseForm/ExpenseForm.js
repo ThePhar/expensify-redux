2d1b75509827ff1d6c490d7e6f43b062560e67a4
Zach Parks <zach@alliware.com>
2019-07-28 04:18:16 -0500

    have most of basic form setup for add expense page

---
+++ src/components/ExpenseForm/ExpenseForm.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default class ExpenseForm extends React.Component {
+    state = {
+        description: "",
+        note: "",
+        amount: ""
+    };
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(() => ({ description }));
+    };
+    onNoteChange = (e) => {
+        const note = e.target.value;
+        this.setState(() => ({ note }));
+    };
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+        if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(() => ({ amount }));
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        autoFocus
+                        required
+                        value={this.state.description}
+                        onChange={this.onDescriptionChange}
+                    /><br />
+                    <input
+                        type="text"
+                        placeholder="Amount"
+                        value={this.state.amount}
+                        onChange={this.onAmountChange}
+                    /><br />
+                    <textarea
+                        placeholder="Add a note for your expense."
+                        value={this.state.note}
+                        onChange={this.onNoteChange}
+                    /><br />
+                    <button>Add Expense</button>
+                </form>
+            </div>
+        );
+    }
+}
