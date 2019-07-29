29362c45a446c6213e2e901c03b91f0371f40a35
Zach Parks <zach@alliware.com>
2019-07-29 01:14:59 -0500

    removed dummy data in index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ import "react-dates/lib/css/_datepicker.css";
 
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
 
 import "normalize-css/normalize.css";
 import "./index.scss";
 
 const store = configureStore();
-
-// TODO: Remove dummy data.
-store.dispatch(addExpense({ description: "Rent", amount: 85000 }));
-store.dispatch(addExpense({ description: "Water bill", amount: 3000, createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Gas bill", amount: 15000, createdAt: -1000 }));
 
 const app = (
     <Provider store={store}>
